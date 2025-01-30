eac3c08a438777be132766d6efbe2c6df6725eb8
D.L. Molineu <DLMolineu@WoodmenLife.org>
2025-01-30 09:55:09 -0600

    Updated code the colorize each iteration of squares, needed to reverse the order of processing the iterations to get the image to render as desired.

--- src/main/java/com/dlmol/sierpinski/SierpinskiApp.java
+++ src/main/java/com/dlmol/sierpinski/SierpinskiApp.java
@@ -10,12 +10,15 @@ import java.util.stream.IntStream;
 public class SierpinskiApp {
 
     private static final String EXTN = "png";
+    private static final Color[] COLORS =
+//            {Color.PINK, Color.CYAN, Color.GREEN, Color.YELLOW, Color.MAGENTA, Color.ORANGE, Color.WHITE};
+            {Color.RED, Color.PINK, Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.WHITE};
 
     public static void main(String[] args) {
         long startMs = System.currentTimeMillis();
         final String imagesDirName = "sierpinski_images";
         processImagesDir(imagesDirName);
-        int maxIterations = 15;
+        int maxIterations = 6;
         IntStream.rangeClosed(1, maxIterations).forEach(maxI -> {
             String filenameBase = imagesDirName + "/sierpinski_carpet_" + maxI;
             String filename = filenameBase + "." + EXTN;
@@ -26,7 +29,8 @@ public class SierpinskiApp {
             g.setColor(Color.BLACK);
             g.fillRect(0, 0, size, size);
             g.setColor(Color.WHITE);
-            drawCarpet(g, 0, 0, size, maxI);
+//            drawCarpet(g, 0, 0, size, maxI);
+            drawCarpetReverse(g, 0, 0, size, maxI);
             g.dispose();
 
             try {
@@ -49,8 +53,7 @@ public class SierpinskiApp {
             }
         }
     }
-
-    private static int drawCarpet(Graphics2D g, int startX, int startY, int size, int maxIterations) {
+    private static void drawCarpet(Graphics2D g, int startX, int startY, int size, int maxIterations) {
         int newSize = size / 3;
         int i = 0;
         while (newSize >= 1 && i++ < maxIterations) {
@@ -63,21 +66,21 @@ public class SierpinskiApp {
             }
             newSize /= 3;
         }
-        return i;
     }
 
-//    private static void drawCarpet(Graphics2D g, int x, int y, int size) {
-//        if (size < 3) {
-//            return;
-//        }
-//        int newSize = size / 3;
-//        g.fillRect(x + newSize, y + newSize, newSize, newSize);
-//        for (int i = 0; i < 3; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                if (i != 1 || j != 1) {
-//                    drawCarpet(g, x + i * newSize, y + j * newSize, newSize);
-//                }
-//            }
-//        }
-//    }
+    private static void drawCarpetReverse(Graphics2D g, int startX, int startY, int size, int maxIterations) {
+        int newSize = 1;
+        int i = maxIterations;
+        while (newSize <= size && i-- > 0) {
+            for (int x = 0; x < size; x += newSize) {
+                for (int y = 0; y < size; y += newSize) {
+                    if ((x / newSize) % 3 == 1 && (y / newSize) % 3 == 1) {
+                        g.setColor(COLORS[Math.min(i, COLORS.length - 1)]);
+                        g.fillRect(startX + x, startY + y, newSize, newSize);
+                    }
+                }
+            }
+            newSize *= 3;
+        }
+    }
 }
